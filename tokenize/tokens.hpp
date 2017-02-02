eac0c1478cfafffe8f41e02ec408ac83685a0368
stryku <stryku2393@gmail.com>
2017-02-02 23:39:55 +0100

    introduce basic tokens

---
+++ tokenize/tokens.hpp
@@ -0,0 +1,28 @@
+#pragma once
+
+#include "string.hpp"
+
+namespace cai
+{
+    namespace tokens
+    {
+        using tok_mov = decltype("mov"_s);
+        using tok_add = decltype("add"_s);
+        using tok_push = decltype("push"_s);
+        using tok_pop = decltype("pop"_s);
+        using tok_sub = decltype("sub"_s);
+        using tok_mul = decltype("mul"_s);
+        using tok_cmp = decltype("cmp"_s);
+
+        using tok_byte = decltype("BYTE"_s);
+        using tok_word = decltype("WORD"_s);
+        using tok_dword = decltype("DWORD"_s);
+        using tok_ptr = decltype("PTR"_s);
+
+        using tok_square_bracket_open = decltype("["_s);
+        using tok_square_bracket_close = decltype("]"_s);
+        using tok_minus = decltype("-"_s);
+        using tok_plus = decltype("+"_s);
+        using tok_comma = decltype(","_s);
+    }
+}
