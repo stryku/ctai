c013f733de0dccb35fe912d79907fd0961d53df8
stryku <stryku2393@gmail.com>
2017-02-02 23:29:37 +0100

    added basic string operations

--- string.hpp
+++ string.hpp
@@ -13,6 +13,9 @@ namespace cai
         return string<chars...>{};
     }
 
+    //
+    // to_string
+    //
     namespace details
     {
         template <typename str>
@@ -28,6 +31,9 @@ namespace cai
     template <typename s>
     using to_string = typename details::to_string_impl<s>::type;
 
+    //
+    //string_eq
+    //
     namespace details
     {
         template <typename, typename, bool eq_size>
@@ -62,30 +68,68 @@ namespace cai
     template <typename ls, typename rs>
     constexpr auto string_eq = details::string_eq_impl<ls, rs, to_string<ls>::size == to_string<rs>::size>::value;
 
+    //
+    //string_front
+    //
+    namespace details
+    {
+        template <typename>
+        struct string_front_impl;
+
+        template <char front, char ...chars>
+        struct string_front_impl<string<front, chars...>>
+        {
+            static constexpr auto value = front;
+        };
+    }
+
+    template <typename s>
+    constexpr auto string_front = details::string_front_impl<s>::value;
+
+    //
+    //string_append
+    //
+    namespace details
+    {
+        template <typename, char>
+        struct string_append_impl;
+
+        template <char ...chars, char value>
+        struct string_append_impl<string<chars...>, value>
+        {
+            using type = string<chars..., value>;
+        };
+    }
+
+    template <typename s, char value>
+    using string_append = typename details::string_append_impl<s, value>::type;
+
+    //
+    //string_pop_front
+    //
+    namespace details
+    {
+        template <typename>
+        struct string_pop_front_impl;
+
+        template <char front, char ...chars>
+        struct string_pop_front_impl<string<front, chars...>>
+        {
+            using type = string<chars...>;
+        };
+    }
+
+    template <typename s>
+    using string_pop_front = typename details::string_pop_front_impl<s>::type;
+
+
     namespace tests
     {
-        static_assert(
-                string_eq<string<'a','b'>, string<'a','b'>>
-                ,"");
-
-        static_assert(
-                string_eq<string<'a','b'>, decltype("ab"_s)>
-                ,"");
-
-        static_assert(
-                !string_eq<string<'a','b'>, string<'A','b'>>
-                ,"");
-
-        static_assert(
-                !string_eq<string<'a','b','c'>, string<'a','b'>>
-                ,"");
-
-        static_assert(
-                !string_eq<string<'a','b'>, string<'A','b'>>
-                ,"");
-
-        static_assert(
-                !string_eq<string<'a','b'>, string<'a','b','c'>>
-                ,"");
+        static_assert(string_eq<string<'a','b'>, string<'a','b'>>, "");
+        static_assert(string_eq<string<'a','b'>, decltype("ab"_s)>, "");
+        static_assert(!string_eq<string<'a','b'>, string<'A','b'>>, "");
+        static_assert(!string_eq<string<'a','b','c'>, string<'a','b'>>, "");
+        static_assert(!string_eq<string<'A','b'>, string<'a','b'>>, "");
+        static_assert(!string_eq<string<'a','b'>, string<'a','b','c'>>, "");
     }
 }
