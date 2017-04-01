f73dd7beb66ce35b3b945a873faf6da6f3503bc9
stryku <stryku2393@gmail.com>
2017-04-02 00:58:44 +0200

    introduce free_block

--- memory/memory_metadata.hpp
+++ memory/memory_metadata.hpp
@@ -1,6 +1,7 @@
 #pragma once
 
 #include "containers/values_container.hpp"
+#include "utils/bad_value.hpp"
 #include "tuple/tuple.hpp"
 
 #include <cstddef>
@@ -32,9 +33,7 @@ namespace ctai
             using create = memory_metadata<values_container_n::create_with_val<size, false>,
                                            tuple_n::tuple<>>;
 
-            //
-            //find_allocated_block
-            //
+
             namespace details
             {
                 template <size_t desired_ptr>
@@ -43,7 +42,13 @@ namespace ctai
                     template <typename block>
                     static constexpr auto value = (block::ptr == desired_ptr);
                 };
+            }
 
+            //
+            //find_allocated_block
+            //
+            namespace details
+            {
                 template <typename metadata, size_t ptr>
                 struct find_allocated_block_impl;
 
@@ -96,26 +101,39 @@ namespace ctai
             //
             namespace details
             {
-                template <typename metadata, size_t ptr, size_t size>
+                template <typename used_bytes, typename allocated_blocks, size_t iterator>
+                struct free_and_erase_block
+                {
+                    using block_metadata = tuple_n::get<allocated_blocks, iterator>;
+                    using new_used_bytes = values_container_n:::set_val<used_bytes,
+                                                                        block_metadata::ptr,
+                                                                        block_metadata::size,
+                                                                        false>;
+                    using new_allocated_blocks = tuple_n::erase<allocated_blocks, iterator>;
+
+                    using result = memory_metadata<new_used_bytes, new_allocated_blocks>;
+                };
+
+                template <typename metadata, size_t ptr>
                 struct free_block_impl;
 
                 template <typename used_bytes,
                           typename allocated_blocks,
-                          size_t ptr,
-                          size_t size>
+                          size_t ptr>
                 struct free_block_impl<memory_metadata<used_bytes,
                                                        allocated_blocks>,
-                                       ptr,
-                                       size>
+                                       ptr>
                 {
-                    using new_used_bytes = values_container_n::set_val<used_bytes, ptr,size, true>;
-                    using new_allocated_blocks = std::conditional_t<(size > 0),
-                                                                    tuple_n::append<allocated_blocks, allocated_block<ptr, size>>,
-                                                                    allocated_blocks>;
+                    static constexpr auto found_it = tuple_n::find_if_it<allocated_blocks, find_allocated_block_predicate>;
 
-                    using result = memory_metadata<new_used_bytes, new_allocated_blocks>;
+                    using result = std::conditional_t<found_it == utils::bad_value,
+                                                      memory_metadata,
+                                                      free_and_erase_block<used_bytes, allocated_blocks, found_it>::result>;
                 };
             }
+
+            template <typename metadata, size_t ptr>
+            using free_block = typename details::free_block_impl<metadata, ptr>::result;
         }
     }
 }
