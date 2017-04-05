3a3c3d7deaf3c4b4b43fd18e65f8400bea903790
stryku <stryku2393@gmail.com>
2017-04-05 22:17:43 +0200

    inotrduce memory::memory_alloc

--- memory/memory_alloc.hpp
+++ memory/memory_alloc.hpp
@@ -1,17 +1,28 @@
 #pragma once
 
 #include "memory/memory.hpp"
+#include "memory/memory_metadata.hpp"
 
 #include <cstddef>
 
 namespace ctai
 {
-    namespace details
+    namespace memory
     {
-        template<typename metadata, size_t size>
-        struct memory_alloc_impl;
+        namespace details
+        {
+            template<typename metadata_t, size_t size>
+            struct memory_alloc_impl
+            {
+                static constexpr auto found_block_ptr = metadata::find_unused_block_of_size<metadata_t, size>;
 
-        template<typename metadata, size_t size>
-        struct memory_alloc_impl;
+                using result_metadata = std::conditional_t<found_block_ptr == utils::bad_value,
+                                                           metadata_t,
+                                                           metadata::reserve_block<metadata_t, found_block_ptr, size>>;
+
+                static constexpr auto result_ptr = found_block_ptr != utils::bad_value ? found_block_ptr
+                                                                                       : 0;
+            };
+        }
     }
 }
