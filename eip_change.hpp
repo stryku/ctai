046ea6cedb0efef4a4f13e6edae198f9a4b6aac5
stryku <stryku2393@gmail.com>
2017-04-09 01:39:05 +0200

    introduce create_thread matcher

--- eip_change.hpp
+++ eip_change.hpp
@@ -33,6 +33,7 @@ namespace ctai
         template <> constexpr size_t eip_change<inst::id_t::CALL_VAL> = 2;
 
         template <> constexpr size_t eip_change<inst::id_t::SYS_EXIT_THREAD> = 1;
+        template <> constexpr size_t eip_change<inst::id_t::SYS_CREATE_THREAD> = 1;
     }
 
     template <inst::id_t instruction>
