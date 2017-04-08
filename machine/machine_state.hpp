a1b47e8a155a5a83da27a0d99d53da5da436da5d
stryku <stryku2393@gmail.com>
2017-04-08 20:21:25 +0200

    introduce execute_thread

--- machine/machine_state.hpp
+++ machine/machine_state.hpp
@@ -6,11 +6,12 @@ namespace ctai
 {
     namespace machine
     {
-        template <typename memory_t, typename threads_t, size_t time_v>
+        template <typename memory_t, typename opcodes_t, typename threads_t, size_t time_v>
         struct state
         {
             using memory = memory_t;
             using threads = threads_t;
+            using opcodes = opcodes_t;
             static constexpr auto time = time_v;
             static constexpr auto next_time = time_v + 1;
         };
