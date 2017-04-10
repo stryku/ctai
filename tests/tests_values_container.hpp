19a9d50cac5087c43ac1e9ba07e056b4513f806c
stryku <stryku2393@gmail.com>
2017-04-10 21:08:37 +0200

    added tests for get and bytes_to_value

--- tests/tests_values_container.hpp
+++ tests/tests_values_container.hpp
@@ -7,9 +7,9 @@
 namespace ctai::tests::values_container
 {
     template<auto ...values>
-    using con = values_container_n::values_container<values...>;
+    using vc = values_container_n::values_container<values...>;
 
-    using test_con = con<6, 7, 8>;
+    using test_con = vc<6, 7, 8>;
 
     namespace test_size
     {
@@ -18,13 +18,13 @@ namespace ctai::tests::values_container
 
     namespace test_pop_front
     {
-        using expected = con<7, 8>;
+        using expected = vc<7, 8>;
         ASSERT_EQ_T(values_container_n::pop_front<test_con>, expected);
     }
 
     namespace test_append
     {
-        using expected = con<6, 7, 8, 9>;
+        using expected = vc<6, 7, 8, 9>;
         using test_type = values_container_n::append<test_con, 9>;
         ASSERT_EQ_T(test_type, expected);
     }
@@ -35,17 +35,17 @@ namespace ctai::tests::values_container
         {
             using expected = test_con;
 
-            using test_type = values_container_n::merge<test_con, con<>>;
+            using test_type = values_container_n::merge<test_con, vc<>>;
             ASSERT_EQ_T(test_type, expected);
 
-            using test_type2 = values_container_n::merge<con<>, test_con>;
+            using test_type2 = values_container_n::merge<vc<>, test_con>;
             ASSERT_EQ_T(test_type2, expected);
         }
 
         namespace merge_some
         {
-            using expected = con<6, 7, 8, 9, 0>;
-            using test_type = values_container_n::merge<test_con, con<9, 0>>;
+            using expected = vc<6, 7, 8, 9, 0>;
+            using test_type = values_container_n::merge<test_con, vc<9, 0>>;
             ASSERT_EQ_T(test_type, expected);
         }
     }
@@ -60,7 +60,7 @@ namespace ctai::tests::values_container
         namespace take_0
         {
             using expected_rest = test_con;
-            using expected_taken = con<>;
+            using expected_taken = vc<>;
             using test_type = values_container_n::take<0, test_con>;
             ASSERT_EQ_T(test_type::taken_container, expected_taken);
             ASSERT_EQ_T(test_type::rest_container, expected_rest);
@@ -68,8 +68,8 @@ namespace ctai::tests::values_container
 
         namespace take_some
         {
-            using expected_rest = con<8>;
-            using expected_taken = con<6, 7>;
+            using expected_rest = vc<8>;
+            using expected_taken = vc<6, 7>;
             using test_type = values_container_n::take<2, test_con>;
             ASSERT_EQ_T(test_type::taken_container, expected_taken);
             ASSERT_EQ_T(test_type::rest_container, expected_rest);
@@ -89,28 +89,28 @@ namespace ctai::tests::values_container
         {
             namespace t1
             {
-                using expected = con<3, 7, 8>;
+                using expected = vc<3, 7, 8>;
                 using test_type = values_container_n::set<0, test_con, 3>;
                 ASSERT_EQ_T(test_type, expected);
             }
 
             namespace t2
             {
-                using expected = con<3, 4, 8>;
+                using expected = vc<3, 4, 8>;
                 using test_type = values_container_n::set<0, test_con, 3, 4>;
                 ASSERT_EQ_T(test_type, expected);
             }
 
             namespace t3
             {
-                using expected = con<3, 4, 5>;
+                using expected = vc<3, 4, 5>;
                 using test_type = values_container_n::set<0, test_con, 3, 4, 5>;
                 ASSERT_EQ_T(test_type, expected);
             }
 
             namespace t4
             {
-                using expected = con<6, 4, 5>;
+                using expected = vc<6, 4, 5>;
                 using test_type = values_container_n::set<1, test_con, 4, 5>;
                 ASSERT_EQ_T(test_type, expected);
             }
@@ -130,21 +130,21 @@ namespace ctai::tests::values_container
         {
             namespace t1
             {
-                using expected = con<7, 8>;
+                using expected = vc<7, 8>;
                 using test_type = values_container_n::drop_front<test_con, 1>;
                 ASSERT_EQ_T(test_type, expected);
             }
 
             namespace t2
             {
-                using expected = con<8>;
+                using expected = vc<8>;
                 using test_type = values_container_n::drop_front<test_con, 2>;
                 ASSERT_EQ_T(test_type, expected);
             }
 
             namespace t3
             {
-                using expected = con<>;
+                using expected = vc<>;
                 using test_type = values_container_n::drop_front<test_con, 3>;
                 ASSERT_EQ_T(test_type, expected);
             }
@@ -153,25 +153,71 @@ namespace ctai::tests::values_container
 
     namespace test_get
     {
-        namespace t1
+        namespace get_1
         {
-            constexpr auto expected = 6;
-            constexpr auto result = values_container_n::get_1<test_con, 0>;
-            ASSERT_EQ(result, expected);
+            namespace t1
+            {
+                constexpr auto expected = 6;
+                constexpr auto result = values_container_n::get_1<test_con, 0>;
+                ASSERT_EQ(result, expected);
+            }
+
+            namespace t2
+            {
+                constexpr auto expected = 7;
+                constexpr auto result = values_container_n::get_1<test_con, 1>;
+                ASSERT_EQ(result, expected);
+            }
+
+            namespace t3
+            {
+                constexpr auto expected = 8;
+                constexpr auto result = values_container_n::get_1<test_con, 2>;
+                ASSERT_EQ(result, expected);
+            }
         }
 
-        namespace t2
+        namespace get_2
         {
-            constexpr auto expected = 7;
-            constexpr auto result = values_container_n::get_1<test_con, 1>;
-            ASSERT_EQ(result, expected);
+            namespace t1
+            {
+                using expected = vc<6, 7>;
+                using result = values_container_n::get_2<test_con, 0>;
+                ASSERT_EQ_T(result, expected);
+            }
+
+            namespace t2
+            {
+                using expected = vc<7, 8>;
+                using result = values_container_n::get_2<test_con, 1>;
+                ASSERT_EQ_T(result, expected);
+            }
         }
 
-        namespace t3
+        namespace get_4
         {
-            constexpr auto expected = 8;
-            constexpr auto result = values_container_n::get_1<test_con, 2>;
-            ASSERT_EQ(result, expected);
+            using test_vc_get_4 = vc<1,2,3,4,5,6>;
+
+            namespace t1
+            {
+                using expected = vc<1,2,3,4>;
+                using result = values_container_n::get_4<test_vc_get_4, 0>;
+                ASSERT_EQ_T(result, expected);
+            }
+
+            namespace t2
+            {
+                using expected = vc<2,3,4,5>;
+                using result = values_container_n::get_4<test_vc_get_4, 1>;
+                ASSERT_EQ_T(result, expected);
+            }
+
+            namespace t3
+            {
+                using expected = vc<3,4,5,6>;
+                using result = values_container_n::get_4<test_vc_get_4, 2>;
+                ASSERT_EQ_T(result, expected);
+            }
         }
     }
 
@@ -196,28 +242,28 @@ namespace ctai::tests::values_container
     {
         namespace create_with_val_zero_size
         {
-            using expected = con<>;
+            using expected = vc<>;
             using test_type = values_container_n::create_with_val<0, 11>;
             ASSERT_EQ_T(test_type, expected);
         }
 
         namespace create_with_val_one_size
         {
-            using expected = con<3>;
+            using expected = vc<3>;
             using test_type = values_container_n::create_with_val<1, 3>;
             ASSERT_EQ_T(test_type, expected);
         }
 
         namespace create_with_val_some_size
         {
-            using expected = con<11, 11, 11>;
+            using expected = vc<11, 11, 11>;
             using test_type = values_container_n::create_with_val<3, 11>;
             ASSERT_EQ_T(test_type, expected);
         }
 
         namespace create_with_val_bool
         {
-            using expected = con<true, true, true>;
+            using expected = vc<true, true, true>;
             using test_type = values_container_n::create_with_val<3, true>;
             ASSERT_EQ_T(test_type, expected);
         }
@@ -235,9 +281,38 @@ namespace ctai::tests::values_container
 
         namespace set_val_one_count
         {
-            using expected = con<3, 7,8>;
+            using expected = vc<3, 7,8>;
             using test_result = values_container_n::set_val<test_con, 0, 1, 3>;
             ASSERT_EQ_T(test_result, expected);
+        }
+    }
+
+    namespace tests_bytes_to_value
+    {
+        using test_vc_bytes_to_value = vc<1,2,3,4,5,6>;
+
+        namespace bytes_to_value_1
+        {
+            constexpr uint8_t expected = 1;
+            constexpr auto result = values_container_n::bytes_to_value_1<test_vc_bytes_to_value>;
+
+            ASSERT_EQ(expected, result);
+        }
+
+        namespace bytes_to_value_2
+        {
+            constexpr uint16_t expected = 0x0102;
+            constexpr auto result = values_container_n::bytes_to_value_2<test_vc_bytes_to_value>;
+
+            ASSERT_EQ(expected, result);
+        }
+
+        namespace bytes_to_value_4
+        {
+            constexpr uint32_t expected = 0x01020304;
+            constexpr auto result = values_container_n::bytes_to_value_4<test_vc_bytes_to_value>;
+
+            ASSERT_EQ(expected, result);
         }
     }
 }
