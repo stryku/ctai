5cdc33bda202870b2b4bda6a492420d1d2a189df
stryku <stryku2393@gmail.com>
2017-04-10 21:34:40 +0200

    introduce memory tests

--- tests/tests_main.hpp
+++ tests/tests_main.hpp
@@ -4,5 +4,6 @@
 #include "tests/tests_tuple.hpp"
 #include "tests/tests_memory_metadata.hpp"
 #include "tests/tests_memory_alloc.hpp"
+#include "tests/tests_memory.hpp"
 
 #include "instructions/tests_instructions.hpp"
