5da3d32473b74febb249043943a559a0a9711a85
stryku <stryku2393@gmail.com>
2017-04-10 18:29:27 +0200

    introduce get_*

--- memory/memory.hpp
+++ memory/memory.hpp
@@ -26,5 +26,44 @@ namespace ctai
 
         template <size_t size>
         using memory_create = memory<size>;
+
+
+        //
+        //get_8
+        //
+        template <typename memory_t, size_t ptr>
+        constexpr auto get_8 = values_container_n::get_1<typename memory_t::mem, ptr>;
+
+        //
+        //get_16
+        //
+        namespace details
+        {
+            template <typename memory_t, size_t ptr>
+            struct get_16_impl
+            {
+                using bytes = values_container_n::get_2<typename memory_t::mem, ptr>;
+                static constexpr auto result = values_container_n::bytes_to_value_2<bytes>;
+            };
+        }
+
+        template <typename memory_t, size_t ptr>
+        constexpr auto get_16_impl = details::get_16_impl<memory_t, ptr>::result;
+
+        //
+        //get_32
+        //
+        namespace details
+        {
+            template <typename memory_t, size_t ptr>
+            struct get_32_impl
+            {
+                using bytes = values_container_n::get_4<typename memory_t::mem, ptr>;
+                static constexpr auto result = values_container_n::bytes_to_value_4<bytes>;
+            };
+        }
+
+        template <typename memory_t, size_t ptr>
+        constexpr auto get_32 = details::get_32_impl<memory_t, ptr>::result;
     }
 }
