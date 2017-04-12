a480eda7c8b64d1c329b1ccb27966030f0548058
stryku <stryku2393@gmail.com>
2017-04-12 20:16:43 +0200

    introduce sys_create_thread in kernel

--- kernel/thread.hpp
+++ kernel/thread.hpp
@@ -6,9 +6,27 @@ namespace ctai
 {
     namespace include
     {
-        using exit_thread = decltype(
+        using sys_exit_thread = decltype(
         ":sys_exit_thread "
                 "sys_exit_thread "_s);
+
+        using sys_create_thread = decltype(
+        ":sys_create_thread "
+                "push ebx "
+                "push ecx "
+                "push edx "
+
+                "mov ebx , DWORD PTR [ esp + 12 ] "//void* start_point\n
+                "mov ecx , DWORD PTR [ esp + 8 ] "//priority
+                "mov edx , DWORD PTR [ esp + 8 ] "//arg
+
+                "sys_create_thread"
+
+                "pop edx "
+                "pop ecx "
+                "pop ebx "
+
+                "ret"_s);
     }
 
 }
