e4fbb514b59c886d59737767916acd6c3248e36c
stryku <stryku2393@gmail.com>
2017-04-16 21:10:23 +0200

    introduce join_thread helper

--- kernel/thread.hpp
+++ kernel/thread.hpp
@@ -30,8 +30,31 @@ namespace ctai
 
                 "ret "_s);
 
+        using sys_is_thread_running = decltype(
+        ":sys_is_thread_running "
+                "mov eax , DWORD PTR [ esp + 4 ] "//thread id
+                "sys_is_thread_running "
+                "ret "_s
+        );
+
+        using join_thread = decltype(
+        ":join_thread "
+                "mov eax , DWORD PTR [ esp + 4 ] " //thread id
+                "push eax "
+
+                ":_jt_check_loop " //wait while thread is running
+                    "call .sys_is_thread_running "
+                    "cmp eax , 0 "
+                    "jne ._jt_check_loop "
+
+                "add esp , 4 " //cleanup stack
+                "ret "_s
+        );
+
         using thread = ctai::declare_code<sys_exit_thread,
-                                          sys_create_thread>;
+                                          sys_create_thread,
+                                          sys_is_thread_running,
+                                          join_thread>;
 
     }
 
