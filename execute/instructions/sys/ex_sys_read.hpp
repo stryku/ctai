be672604b4a0d3368acfde7add307f3bd50dbdc1
stryku <stryku2393@gmail.com>
2017-04-17 23:14:40 +0200

    implemented ex_sys_read

--- execute/instructions/sys/ex_sys_read.hpp
+++ execute/instructions/sys/ex_sys_read.hpp
@@ -6,7 +6,7 @@
 #include "execute/eip_adjuster.hpp"
 #include "thread/thread.hpp"
 #include "machine/machine_state.hpp"
-#include "io/output.hpp"
+#include "io/input.hpp"
 
 #include <cstddef>
 #include <io/io.hpp>
@@ -18,18 +18,19 @@ namespace ctai
         template <typename thread_t, typename machine_state_t, size_t ...rest_of_opcodes>
         struct ex_instruction<thread_t,
                 machine_state_t,
-                inst::to_size<inst::id_t::SYS_WRITE>,
+                inst::to_size<inst::id_t::SYS_READ>,
                 rest_of_opcodes...>
         {
-            using final_registers = adjust_eip<typename thread_t::registers, inst::id_t::SYS_WRITE>;
+            using current_input = typename machine_state_t::input;
+            static constexpr auto read_char = io::input::front<current_input>;
+
+            using registers_after_al = set_reg<typename thread_t::registers, regs::id_t::AL, read_char>;
+
+            using final_registers = adjust_eip<registers_after_al, inst::id_t::SYS_READ>;
             using result_thread = thread::set_registers<thread_t, final_registers>;
 
-            static constexpr auto character = static_cast<char>(get_reg<final_registers, regs::id_t::AL>);
-
-            using next_output = io::write<typename machine_state_t::output,
-                    character>;
-
-            using result_machine_state = machine::set_output<machine_state_t, next_output>;
+            using next_input = io::input::pop<current_input>;
+            using result_machine_state = machine::set_input<machine_state_t, next_input>;
         };
     }
 }
