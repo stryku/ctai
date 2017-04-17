d5132cd5205b6b1a6e65fd7ea8188387646359ff
stryku <stryku2393@gmail.com>
2017-04-17 23:59:57 +0200

    introduce is_digit in the kernel

--- kernel/utils.hpp
+++ kernel/utils.hpp
@@ -8,7 +8,20 @@ namespace ctai
     {
         using is_digit = decltype(
         ":is_digit "
-                ""
+                "cmp al , '0' "
+                "jl .__uid_no "
+
+                "cmp al , '9' "
+                "jg .__uid_no "
+
+                "mov al , 1 "
+                "jmp .__uid_end "
+
+                ":__uid_no "
+                "mov al , 0 "
+
+                ":__uid_end "
+                "ret "_s
         );
     }
 }
