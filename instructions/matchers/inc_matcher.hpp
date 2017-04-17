b06e74b077a7c08e0e2406eeca6a5e95b659596f
stryku <stryku2393@gmail.com>
2017-04-18 00:42:01 +0200

    introduce inc_matcher

---
+++ instructions/matchers/inc_matcher.hpp
@@ -0,0 +1,35 @@
+#pragma once
+
+#include "tuple.hpp"
+#include "tokenize/tokens.hpp"
+#include "instructions/ids_vaules.hpp"
+#include "containers/values_container.hpp"
+#include "eip_change.hpp"
+
+namespace ctai
+{
+    namespace details
+    {
+        // inc register
+        template <typename reg_token, typename ...rest_of_tokens>
+        struct matcher_impl<tuple<
+                tokens::tok_inc,
+                reg_token,
+                rest_of_tokens...>>
+        {
+            static constexpr auto instruction_type = inst::id_t::INC;
+
+            using instruction = values_container_n::values_container<
+                    inst::to_size<instruction_type>,
+                    token_to_reg_opcode<reg_token>>;
+
+            using instruction_tokens = tuple<
+                    tokens::tok_inc,
+                    reg_token>;
+
+            static constexpr auto eip_change = get_eip_change<instruction_type>;
+
+            using rest_of_tokens_t = tuple<rest_of_tokens...>;
+        };
+    }
+}
