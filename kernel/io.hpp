52e3c83c9b04611d26adfe37f8f05f750f8d5312
stryku <stryku2393@gmail.com>
2017-04-18 00:37:16 +0200

    introduce getline in the kernel

--- kernel/io.hpp
+++ kernel/io.hpp
@@ -15,5 +15,28 @@ namespace ctai
         ":sys_read "
                 "sys_read "
                 "ret "_s);
+
+        using getline = decltype(
+        ":getline "
+                "push ecx "
+
+                "mov ecx , 0 " //chars counter
+                "mov ah , al " // move delimeter to ah
+
+            ":__iog_read_loop "
+                "sys_read "
+                "cmp ah , al "
+                "je .__iog_finish "
+                "mov BYTE PTR [ ebx + ecx ] , al "
+                "inc ecx "
+                "jmp __iog_read_loop "
+
+            ":__iog_finish "
+                "mov eax , ecx "
+
+                "pop ecx "
+
+                "ret "_s
+        );
     }
 }
