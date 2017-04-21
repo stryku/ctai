520929df7aef7a9d98bcd36916bb88aadbfb0749
stryku <stryku2393@gmail.com>
2017-04-22 00:13:45 +0200

    introduce sub and add reg_reg

--- execute/instructions/ex_add.hpp
+++ execute/instructions/ex_add.hpp
@@ -7,20 +7,47 @@ namespace ctai
 {
     namespace execute2
     {
+        namespace details
+        {
+            template <typename thread_t,
+                      typename machine_state_t, size_t reg1, size_t val>
+            struct ex_add_impl
+            {
+                using registers = typename thread_t::registers;
+
+                static constexpr auto val1 = get_reg<registers, regs::to_id<reg1>>;
+
+                using new_regs_state = set_reg<registers, regs::to_id<reg1>, static_cast<uint32_t>(val1 + val)>;
+
+                using final_regs_state = adjust_eip<new_regs_state, inst::id_t::ADD_REG_VAL>;
+
+                using result_thread = thread::set_registers<thread_t, final_regs_state>;
+                using result_machine_state = machine_state_t;
+            };
+        }
+
         template<typename thread_t,
                 typename machine_state_t, size_t reg1, size_t val, size_t ...rest_of_opcodes>
         struct ex_instruction<thread_t, machine_state_t, inst::to_size<inst::id_t::ADD_REG_VAL>, reg1, val, rest_of_opcodes...>
         {
             using registers = typename thread_t::registers;
 
-            static constexpr auto val1 = get_reg<registers, regs::to_id<reg1>>; // get reg1 value
+            using add_result = details::ex_add_impl<thread_t, machine_state_t, reg1, val>;
 
-            using new_regs_state = set_reg<registers, regs::to_id<reg1>, static_cast<uint32_t>(val1 + val)>; //move value to register
+            using result_thread = typename add_result::result_thread;
+            using result_machine_state = typename add_result::result_machine_state;
+        };
 
-            using final_regs_state = adjust_eip<new_regs_state, inst::id_t::ADD_REG_VAL>;
+        template<typename thread_t,
+                 typename machine_state_t, size_t reg1, size_t reg2, size_t ...rest_of_opcodes>
+        struct ex_instruction<thread_t, machine_state_t, inst::to_size<inst::id_t::ADD_REG_REG>, reg1, reg2, rest_of_opcodes...>
+        {
+            using registers = typename thread_t::registers;
 
-            using result_thread = thread::set_registers<thread_t, final_regs_state>;
-            using result_machine_state = machine_state_t;
+            using add_result = details::ex_add_impl<thread_t, machine_state_t, reg1, get_reg<registers, regs::to_id<reg2>>>;
+
+            using result_thread = typename add_result::result_thread;
+            using result_machine_state = typename add_result::result_machine_state;
         };
     }
 }
